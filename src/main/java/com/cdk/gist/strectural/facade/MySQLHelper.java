5b9c9baa6f27bfa491e88f3e6e93e1f467ea72fd
Gupta, Kapil <Kapil.Gupta@cdk.com>
2018-06-25 14:47:13 +0530

    Added code for facade pattern

---
+++ src/main/java/com/cdk/gist/strectural/facade/MySQLHelper.java
@@ -0,0 +1,19 @@
+package com.cdk.gist.strectural.facade;
+
+import java.sql.Connection;
+
+public class MySQLHelper {
+
+	public static Connection getMySqlDBConnection() {
+		// get Mysql DB connection
+		return null;
+	}
+
+	public void generateMySqlPDFReport(String tableName, Connection con) {
+		// get data from table and generate pdf report
+	}
+
+	public void generateMySqlHTMLReport(String tableName, Connection con) {
+		// get data from table and generate pdf report
+	}
+}
